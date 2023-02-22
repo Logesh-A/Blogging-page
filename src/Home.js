9ffc140fa5253477809161c8ae054c1e914c25bf
Logesh-A <logeshwaran200329@gmail.com>
2023-02-22 21:59:40 +0530

    Data using JSON server, routers were set, customHook is created to fetch data, particular blog view.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
+    const { data, error, isPending } = useFetch('http://localhost:5000/blogs');
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            {error && <div>{ error }</div>}
+            {isPending && <div><img src="https://hackernoon.com/images/0*4Gzjgh9Y7Gu8KEtZ.gif" alt="" style={{height:"200px",width:"200px"}}/></div>}
+            {data && <BlogList blogs = { data } title = "All Blogs!"></BlogList>}
         </div>
     );
 }
